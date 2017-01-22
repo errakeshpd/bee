647e721cc506d689079f01d0b28adcb85fe6e93d
Rakesh PD <errakeshpd@gmail.com>
2017-01-22 23:50:19 +0530

    js swal added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   post 'play' => 'games#play'
   get '/flying_bee/:id' => 'games#flying_bee', as: 'flying_bee'
+  get '/game/:id/hitme/:bee_id' => 'games#hitme'
+  get '/rebuild/:id' => 'games#rebuild'
 end
