54a3c652c87d6976afdc63383e2e22ebacc67c45
Rakesh PD <errakeshpd@gmail.com>
2017-01-22 17:59:15 +0530

    bees build added

--- app/models/bee.rb
+++ app/models/bee.rb
@@ -1,3 +1,28 @@
 class Bee < ApplicationRecord
   belongs_to :game
+
+  enum type: ['drone', 'worker', 'queen']
+
+  def self.type(name)
+    value = if name == 'drone'
+      0
+    elsif name == 'worker'
+      1
+    else
+      2
+    end
+    return value
+  end
+
+  def self.life_span(name)
+    value = if name == 'drone'
+      50
+    elsif name == 'worker'
+      75
+    else
+      100
+    end
+    return value
+  end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,23 @@
 class Game < ApplicationRecord
   has_many :bees, dependent: :destroy
   validates :name, presence: true
+
+  after_create :build_bees
+
+
+  def build_bees
+    ['drone', 'worker', 'queen'].each do |bee_type|
+      count = if bee_type == "drone"
+        8
+      elsif bee_type == 'worker'
+        5
+      else
+        1
+      end
+
+      (1..count).each do |b|
+        bees.create(type: Bee.type(bee_type), lifespan: Bee.life_span(bee_type))
+      end
+    end
+  end
 end
