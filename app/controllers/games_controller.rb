594ffef04b15c7dcd3c24093056cf82bf6bceb70
Rakesh PD <errakeshpd@gmail.com>
2017-01-22 17:42:34 +0530

    games controller included

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,23 @@
 class GamesController < ApplicationController
   def index
+    @game = Game.new
   end
 
   def play
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to flying_bee_path(@game)
+    else
+      redirect_to :back
+    end
   end
+
+  def flying_bee
+    @game = Game.find(params[:id])
+  end
+
+  private
+    def game_params
+      params.require(:game).permit(:name)
+    end
 end
