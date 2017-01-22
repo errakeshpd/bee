f019a74403f5c282ff7654ddd7c6bdbaab6d92bf
Rakesh PD <errakeshpd@gmail.com>
2017-01-22 21:22:58 +0530

    bee styles altered

--- app/models/bee.rb
+++ app/models/bee.rb
@@ -25,4 +25,27 @@ class Bee < ApplicationRecord
     return value
   end
 
+  def self.life_span(name)
+    value = if name == 'drone'
+      50
+    elsif name == 'worker'
+      75
+    else
+      100
+    end
+    return value
+  end
+
+  def progressbar(name)
+    value = if name == 'drone'
+      50
+    elsif name == 'worker'
+      75
+    else
+      100
+    end
+
+    progres = (lifespan/value)*100
+    return progres
+  end
 end
