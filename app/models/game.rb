40dedfc61cbf9a6df8de0c0d074f3ff8ecc8d7ea
Rakesh PD <errakeshpd@gmail.com>
2017-01-22 17:42:19 +0530

    form altered

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ApplicationRecord
   has_many :bees, dependent: :destroy
+  validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'games#index'
 
-  get '/play_me' => 'games#play'
+  post 'play' => 'games#play'
+  get '/flying_bee/:id' => 'games#flying_bee', as: 'flying_bee'
 end
